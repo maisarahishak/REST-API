bd8c48326b728acc57b3a7699446bded06f934c1
maisarahishak <maisarah2299@gmail.com>
2022-12-28 03:08:43 +0800

    init

---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\LoginController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\StudentsController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('login', [LoginController::class, 'login']);
+
+// only authenticated user can access to products
+Route::middleware('auth:api')->group(function () {
+    //Route::get('products', [ProductsController::class, 'index']);
+    //Route::get("list",[StudentController::class,'list']);
+});
+
+Route::get("search/{name}",[StudentsController::class,'search']);
+Route::get("paginate",[StudentsController::class,'paginate']); //pagination
+
+Route::apiResource("student", StudentsController::class); // only return name and address
+
